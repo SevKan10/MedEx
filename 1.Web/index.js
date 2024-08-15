4ea2d48524ef59772d7f8e3b54e4ba6a8e171373
KKhang <127233909+SevKan10@users.noreply.github.com>
2024-08-15 11:25:03 +0700

    full code and firebase ok

--- 1.Web/index.js
+++ 1.Web/index.js
@@ -1,73 +1,41 @@
-/* Xử lý tác vụ nút check box và cccd */
-document.addEventListener('DOMContentLoaded', function () 
-{
+/* Xử lý tác vụ nút check box, cccd và registerFunc */
+document.addEventListener('DOMContentLoaded', function() {
     const isChildCheckbox = document.getElementById('isChild');
     const cccdInput = document.getElementById('cccd');
+    const registrationForm = document.getElementById('registrationForm');
 
-    isChildCheckbox.addEventListener('change', function () 
-    {
-        if (isChildCheckbox.checked) 
-        {
+    isChildCheckbox.addEventListener('change', function() {
+        cccdInput.disabled = isChildCheckbox.checked;
+        if (isChildCheckbox.checked) {
             cccdInput.value = ''; // Xóa giá trị trường CCCD
-            cccdInput.disabled = true; // Vô hiệu hóa trường CCCD
-        } else {cccdInput.disabled = false; /* Kích hoạt lại trường CCCD khi checkbox không được chọn*/}
+        }
     });
-});
-/*===========================================================================================================================*/
 
-/*Load hàm gọi registerFunc*/
-document.addEventListener('DOMContentLoaded', function() 
-{
-    document.getElementById('registrationForm').addEventListener('submit', function(event) 
-    {
+    registrationForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Ngăn hành động submit mặc định
         registerFunc(); // Gọi hàm registerFunc
     });
 });
 /*===========================================================================================================================*/
 
-/*Kết nối cơ sở dữ liệu Firebase bằng SDK*/ 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
-// import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
-
-/*Config của FB*/
-const firebaseConfig = 
-{
-    apiKey: "AIzaSyC78Puf9SQHvcQeU79vLN0QRdZZ-kXnWsk",
-    authDomain: "medex-7ff53.firebaseapp.com",
-    projectId: "medex-7ff53",
-    storageBucket: "medex-7ff53.appspot.com",
-    messagingSenderId: "729617022717",
-    appId: "1:729617022717:web:0742a442b5abcfd75d0894"
-}
-
-/*Khởi tạo FB*/
-const app = initializeApp(firebaseConfig);
-// const storage = getStorage(app);
-/*===========================================================================================================================*/
-
 /*Đăng ký*/
-function registerFunc() {
+async function registerFunc() 
+{
     // Khởi tạo biến
-    var fullName = document.getElementById("fullName").value;
-    const CCCD = document.getElementById("cccd").value;
-    var addressCustomer = document.getElementById("address").value;
+    const fullName = document.getElementById("fullName").value;
+    let CCCD = document.getElementById("cccd").value;
+    const addressCustomer = document.getElementById("address").value;
     const phoneNumber = document.getElementById("phone").value;
-    var emailCustomer = document.getElementById("email").value;
-    var dob = document.getElementById("dob").value;
-    var gender = document.querySelector('input[name="Giới tính"]:checked').value;
-    var symptomsCustomer = document.getElementById("symptoms").value;
-    var examDate = document.getElementById("ed").value;
-    var photoInput = document.getElementById("photo");
-    var photoFile = photoInput.files[0]; // Lấy tệp đầu tiên (trong trường hợp chỉ chọn một tệp)
+    const emailCustomer = document.getElementById("email").value;
+    const dob = document.getElementById("dob").value;
+    const gender = document.querySelector('input[name="Giới tính"]:checked').value;
+    const symptomsCustomer = document.getElementById("symptoms").value;
+    const examDate = document.getElementById("ed").value;
+    const photoFile = document.getElementById("photo").files[0]; // Lấy tệp đầu tiên (trong trường hợp chỉ chọn một tệp)
 
-    if (CCCD === "") {
-        CCCD = "Là trẻ em (không có)";
-    }
+    if (CCCD === "") {CCCD = "Là trẻ em (không có)";}
 
-    console.log("Full Name:   ", fullName);
+    /*console.log("Full Name:   ", fullName);
     console.log("CCCD:         ", CCCD);
     console.log("Address:      ", addressCustomer);
     console.log("Phone Number: ", phoneNumber);
@@ -75,26 +43,26 @@ function registerFunc() {
     console.log("Date of Birth:", dob);
     console.log("Gender:       ", gender);
     console.log("Symptoms:     ", symptomsCustomer);
-    console.log("Exam Date:    ", examDate);
+    console.log("Exam Date:    ", examDate);*/
 
     // Kiểm tra nếu có tệp hình ảnh được chọn
-    if (photoFile) 
-    {
-        console.log("File Name:", photoFile.name);
-        console.log("File Size:", photoFile.size);
-        console.log("File Type:", photoFile.type);
+    if (!photoFile) {alert("Vui lòng thêm ảnh!"); return;}
 
-        var reader = new FileReader();
-        reader.onload = function(e) 
-        {
-            var imageDataUrl = e.target.result;
-            console.log("Image Data URL:", imageDataUrl); // URL của hình ảnh đã được mã hóa Base64
-            sendToSheet(fullName, CCCD, addressCustomer, phoneNumber, emailCustomer, dob, gender, symptomsCustomer, examDate, imageDataUrl);
-            sendEmail(emailCustomer, fullName, phoneNumber, CCCD, addressCustomer, symptomsCustomer, examDate);
-        };
+    // Đọc hình ảnh và tải lên Firebase
+    const imageDataUrl = await readFile(photoFile);
+    await saveToFireBase(fullName, CCCD, addressCustomer, phoneNumber, emailCustomer, dob, gender, symptomsCustomer, examDate, photoFile);
+    sendToSheet(fullName, CCCD, addressCustomer, phoneNumber, emailCustomer, dob, gender, symptomsCustomer, examDate, imageDataUrl);
+    sendEmail(emailCustomer, fullName, phoneNumber, CCCD, addressCustomer, symptomsCustomer, examDate);
+}
+/*===========================================================================================================================*/
+function readFile(photoFile) 
+{
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (e) => resolve(e.target.result);
+        reader.onerror = (e) => reject(e);
         reader.readAsDataURL(photoFile);
-    } 
-    else {console.log("No photo selected");alert("Vui lòng thêm ảnh!");}
+    });
 }
 /*===========================================================================================================================*/
 
@@ -113,8 +81,9 @@ function sendEmail(emailCustomer, fullName, phoneNumber, CCCD, addressCustomer, 
         symptoms: symptomsCustomer,
         exam_date: examDate,
     }
-    emailjs.send("service_0295kaf", "template_0i5z4i9", params).then(function(response) {alert("Đăng ký thành công, vui lòng kiểm tra email");},
-    function(error) {console.error('Error sending email:', error);});;
+    emailjs.send("service_0295kaf", "template_0i5z4i9", params)
+        .then(() => alert("Đăng ký thành công, vui lòng kiểm tra email"))
+        .catch((error) => console.error('Error sending email:', error));
 }
 /*===========================================================================================================================*/
 
@@ -135,11 +104,52 @@ function sendToSheet(fullName, CCCD, addressCustomer, phoneNumber, emailCustomer
     formData.append('Ngày Khám',        examDate);
 
     fetch(scriptURL, { method: 'POST', body: formData })
-        .then(response => 
-        {
-            if (response.ok) {console.log("Send success");}
-            else {console.log("Send fail");}
-        })
+        .then(response =>{if (response.ok) {console.log("Send success");}
+        else {console.log("Send fail");}})
         .catch(error => console.error('error!', error.message));
+}
+/*===========================================================================================================================*/
+
+/*Kết nối cơ sở dữ liệu Firebase bằng SDK*/ 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyC78Puf9SQHvcQeU79vLN0QRdZZ-kXnWsk",
+   authDomain: "medex-7ff53.firebaseapp.com",
+   databaseURL: "https://medex-7ff53-default-rtdb.asia-southeast1.firebasedatabase.app",
+   projectId: "medex-7ff53",
+   storageBucket: "medex-7ff53.appspot.com",
+   messagingSenderId: "729617022717",
+   appId: "1:729617022717:web:0742a442b5abcfd75d0894"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+async function saveToFireBase(fullName, CCCD, addressCustomer, phoneNumber, emailCustomer, dob, gender, symptomsCustomer, examDate, photoFile) {
+    const storage = getStorage();
+    const imageRef = storageRef(storage, 'images/' + fullName);
+
+    try {
+        const snapshot = await uploadBytes(imageRef, photoFile);
+        console.log('Image uploaded successfully');
+        const downloadURL = await getDownloadURL(snapshot.ref);
+        await set(ref(db, 'user/' + fullName), {
+            ID_CCCD: CCCD,
+            Address: addressCustomer,
+            Phone: phoneNumber,
+            Email: emailCustomer,
+            Birthday: dob,
+            Gender: gender,
+            Symptoms: symptomsCustomer,
+            Exam_Date: examDate,
+            ImageURL: downloadURL
+        });
+        console.log("Dữ liệu đã được lưu thành công!");
+    } catch (error) {
+        console.error("Lỗi tải lên hình ảnh hoặc lưu dữ liệu: ", error);
+    }
 }
 /*===========================================================================================================================*/
