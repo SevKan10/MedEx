9f3acb6a0cace19f7049505931e9ce3d8d134edb
KKhang <127233909+SevKan10@users.noreply.github.com>
2024-09-08 15:25:53 +0700

    emotion

--- 6. Emotion/chop_mat.py
+++ 6. Emotion/chop_mat.py
@@ -10,8 +10,8 @@ screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Robot Blinking Eyes")
 
 # Tải các hình ảnh (với kích thước 1280x720)
-open_eye = pygame.image.load('mo_mat.png')
-closed_eye = pygame.image.load('nham_mat.png')
+open_eye = pygame.image.load('images/speak1.png')
+closed_eye = pygame.image.load('images/speak2.png')
 
 # Vị trí của mắt trên màn hình (center align)
 eye_x = 0
@@ -33,7 +33,7 @@ while running:
     pygame.display.flip()
     
     # Thời gian ngẫu nhiên cho trạng thái mở mắt
-    open_time = random.uniform(2, 5)  # Thời gian ngẫu nhiên từ 2 đến 5 giây
+    open_time = random.uniform(1, 2)  # Thời gian ngẫu nhiên từ 2 đến 5 giây
     time.sleep(open_time)
     
     # Thực hiện chớp mắt tối đa 2 lần liên tục
@@ -42,13 +42,13 @@ while running:
         screen.fill(background_color)
         screen.blit(closed_eye, (eye_x, eye_y))
         pygame.display.flip()
-        time.sleep(random.uniform(0.05, 0.2))  # Thời gian nhắm mắt ngẫu nhiên từ 0.05 đến 0.2 giây
+        time.sleep(random.uniform(0.05, 0.1))  # Thời gian nhắm mắt ngẫu nhiên từ 0.05 đến 0.2 giây
 
         # Hiển thị hình ảnh mở mắt
         screen.fill(background_color)
         screen.blit(open_eye, (eye_x, eye_y))
         pygame.display.flip()
-        time.sleep(random.uniform(0.05, 0.2))  # Thời gian mở mắt ngẫu nhiên từ 0.05 đến 0.2 giây
+        time.sleep(random.uniform(0.05, 0.1))  # Thời gian mở mắt ngẫu nhiên từ 0.05 đến 0.2 giây
 
 # Kết thúc Pygame
 pygame.quit()
